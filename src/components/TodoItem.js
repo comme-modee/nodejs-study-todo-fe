a23a4b39b56a6301878c4c9a50cfff455a84f1a8
Modee <144014486+comme-modee@users.noreply.github.com>
2024-08-27 10:29:08 +0900

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,7 +9,7 @@ const TodoItem = ({ item, deleteTask, toggleComplete }) => {
       <div className={`todo-content ${item.isComplete ? 'todo-content-done' : ''}`}>{item.task}</div>
       
       <div className="btn-wrapper">
-        <div className="author">by {item.author.name}</div>
+        <div className="author">by {item?.author?.name}</div>
         <button className={`button-status ${item.isComplete ? 'done' : 'ing'}`} onClick={() => toggleComplete(item._id)}><FontAwesomeIcon icon={faCheck}/></button>
         <button className="button-delete" onClick={() => deleteTask(item._id)}><FontAwesomeIcon icon={faMinus}/></button>
       </div>
